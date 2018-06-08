e3aae37c7159afc828e1a3e24d97e0e8b28909ab
Aditya Shinde <shinde.aditya386@gmail.com>
2018-06-08 15:18:18 -0400

    Completed and tested code for device states

--- Summer2018/code/device.py
+++ Summer2018/code/device.py
@@ -1,4 +1,5 @@
 import logging
+import numpy
 from component import CPU, DiskIO, Network
 
 
@@ -8,4 +9,35 @@ class DeviceState:
         Defines a state in the FSM of the device.
     """
 
-    
+    def __init__(self,
+                 cpu_cycles,
+                 disk_cycles,
+                 network_cycles,
+                 noise=0.1,
+                 name="undefined state"):
+
+        self.cpu = CPU(cycles=cpu_cycles)
+        self.disk = DiskIO(cycles=disk_cycles)
+        self.network = Network(cycles=network_cycles)
+
+        self.logger = logging.getLogger(f"{self.__class__.__name__}")
+
+    def get_data(self, start, stop):
+        cpu_samples = self.cpu.get_data(start=start, stop=stop)
+        disk_samples = self.disk.get_data(start=start, stop=stop)
+        network_samples = self.network.get_data(start=start, stop=stop)
+
+        data = numpy.stack([cpu_samples,
+                            disk_samples,
+                            network_samples], axis=1)
+
+        return data
+
+
+class Device:
+
+    """
+        Simulates a device with various states.
+    """
+
+    def __init__(self)
---
+++ Summer2018/tests/test_devicestate.py
@@ -0,0 +1,44 @@
+import unittest
+import sys
+import numpy
+
+sys.path.append("/home/adityas/UGA/SensorWeb/scripts/Summer2018/code")
+
+from device import DeviceState
+
+
+class TestDeviceState(unittest.TestCase):
+
+    def setUp(self):
+        self.send_over_network = DeviceState(cpu_cycles=2,
+                                             network_cycles=10,
+                                             disk_cycles=3,
+                                             name="net_send")
+
+        self.compute = DeviceState(cpu_cycles=10,
+                                   network_cycles=1,
+                                   disk_cycles=1,
+                                   name="compute")
+
+        self.read_file = DeviceState(cpu_cycles=3,
+                                     disk_cycles=10,
+                                     network_cycles=1,
+                                     name="read")
+
+    def test_data_scale(self):
+        for i in range(3):
+            data = self.send_over_network.get_data(start=i, stop=i+1)
+            self.assertTrue(numpy.max(data) <= 1.0 and numpy.min(data) >= 0.0)
+            data = self.compute.get_data(start=i, stop=i+1)
+            self.assertTrue(numpy.max(data) <= 1.0 and numpy.min(data) >= 0.0)
+            data = self.read_file.get_data(start=i, stop=i+1)
+            self.assertTrue(numpy.max(data) <= 1.0 and numpy.min(data) >= 0.0)
+
+    def test_data_shape(self):
+        for i in range(1):
+            data = self.send_over_network.get_data(start=i, stop=i+1)
+            print(data.shape)
+            data = self.compute.get_data(start=i, stop=i+1)
+            print(data.shape)
+            data = self.read_file.get_data(start=i, stop=i+1)
+            print(data.shape)
